fa28c5dd017831dbc562fbcb4de286ce7fec9656
Hawthorn <93634554+Hawthorn0@users.noreply.github.com>
2026-06-24 17:44:05 -0700

    temp

--- js/music.js
+++ js/music.js
@@ -10,6 +10,11 @@ const mp3Files = [
         tags: ["electronic", "spacey", "my favorites"] 
     },
     { 
+        title: "Green Grass Blue Sky", 
+        file: "assets/songs/SG Grass Theme.mp3", 
+        tags: ["electronic", "energetic", "upbeat", "epic", "fantasy", "grassy", "my favorites"] 
+    },
+    { 
         title: "Mercenary", 
         file: "assets/songs/mercenary.mp3", 
         tags: ["rock", "energetic", "epic", "bossfight", "my favorites"] 
